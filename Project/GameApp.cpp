54e2d7d92aabcad7e447ecb3a8cb92bf58e2e43e
C03940926 <C0394@oic.jp>
2022-05-13 10:42:02 +0900

    9-1まで

--- Project/GameApp.cpp
+++ Project/GameApp.cpp
@@ -9,6 +9,14 @@
 
 //INCLUDE
 #include	"GameApp.h"
+#include	"Player.h"
+
+CPlayer		Player;
+
+CCamera		Camera;
+CDirectionalLight	Light;
+
+bool		gdDebug;
 
 /*************************************************************************//*!
 		@brief			アプリケーションの初期化
@@ -20,6 +28,20 @@
 MofBool CGameApp::Initialize(void){
 	//リソース配置ディレクトリの設定
 	CUtilities::SetCurrentDirectory("Resource");
+
+	Camera.SetViewPort();
+	Camera.LookAt(Vector3(0, 6.0f, -17.0f), Vector3(0, 0, -10), Vector3(0, 1, 0));
+	Camera.PerspectiveFov(MOF_ToRadian(60.0f), 1024.0f / 768.0f, 0.01f, 1000.0f);
+	CGraphicsUtilities::SetCamera(&Camera);
+
+	Light.SetDirection(Vector3(-1, -2, 1.5f));
+	Light.SetDiffuse(MOF_COLOR_WHITE);
+	Light.SetAmbient(MOF_COLOR_HWHITE);
+	Light.SetSpeculer(MOF_COLOR_WHITE);
+	CGraphicsUtilities::SetDirectionalLight(&Light);
+
+	Player.Load();
+	Player.Initialize();
 	
 	return TRUE;
 }
@@ -33,6 +55,27 @@ MofBool CGameApp::Initialize(void){
 MofBool CGameApp::Update(void){
 	//キーの更新
 	g_pInput->RefreshKey();
+
+
+
+	Player.Update();
+
+
+	if (g_pInput->IsKeyPush(MOFKEY_F1))
+	{
+		gdDebug = ((gdDebug) ? false : true);
+	}
+
+	float posX = Player.GetPosition().x * 0.4f;
+	CVector3 cpos = Camera.GetViewPosition();
+	CVector3 tpos = Camera.GetTargetPosition();
+	CVector3 vup = CVector3(0, 1, 0);
+	cpos.x = posX;
+	tpos.x = posX;
+	Camera.LookAt(cpos, tpos, vup);
+
+	Camera.Update();
+
 	return TRUE;
 }
 
@@ -49,6 +92,21 @@ MofBool CGameApp::Render(void){
 	// 画面のクリア
 	g_pGraphics->ClearTarget(0.65f,0.65f,0.67f,0.0f,1.0f,0);
 
+	g_pGraphics->SetDepthEnable(true);
+
+	Player.Render();
+
+
+	g_pGraphics->SetDepthEnable(false);
+
+	if (gdDebug)
+	{
+		Player.RenderDebugText();
+	}
+
+
+	
+
 	// 描画の終了
 	g_pGraphics->RenderEnd();
 	return TRUE;
@@ -61,5 +119,8 @@ MofBool CGameApp::Render(void){
 						それ以外	失敗、エラーコードが戻り値となる
 *//**************************************************************************/
 MofBool CGameApp::Release(void){
+
+	Player.Release();
+
 	return TRUE;
 }
